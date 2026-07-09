5e7bdf93878e709fd76e2eeed477ab16274bf024
TVSEIMS <karishmawork26@gmail.com>
2026-07-09 17:41:41 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,7 +18,14 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-resource "azurerm_resource_group" "test" {
-  name     = "rg-terraform-test"
-  location = "centralindia"
+data "azurerm_resource_group" "existing" {
+  name = "rg_Infra_Automation_Dev"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "tvsesttfteststorage04"   # must be globally unique, all lowercase
+  resource_group_name      = data.azurerm_resource_group.existing.name
+  location                 = data.azurerm_resource_group.existing.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
