9b2dd13b05d4eeb68fd4530f3e16b7b93c1f6b7f
TVSEIMS <karishmawork26@gmail.com>
2026-07-09 18:00:59 +0530

    Replace storage account with VM module configuration
    Removed storage account resource and added VM module configuration.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,10 +22,16 @@ data "azurerm_resource_group" "existing" {
   name = "rg_Infra_Automation_Dev"
 }
 
-resource "azurerm_storage_account" "test" {
-  name                     = "tvsesttfteststorage04"   # must be globally unique, all lowercase
-  resource_group_name      = data.azurerm_resource_group.existing.name
-  location                 = data.azurerm_resource_group.existing.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+module "test_vm" {
+  source              = "../../modules/vm"
+  vm_name             = "test-vm-01"
+  vm_size             = "Standard_B1s"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  subnet_id           = "e9a23062-3783-48ff-b4a8-f7814c17bd1e"
+  ssh_public_key      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhkqLmUe0VBhAaRBqxJwQ2TDE7r6pSf91VpFmbh2EY+ infra-automation"
+  tags = {
+    environment = "dev"
+    purpose     = "pipeline-test"
+  }
 }
