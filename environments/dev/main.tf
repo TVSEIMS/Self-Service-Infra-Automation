3d52f9248477948fb9ff01eef211c37d09bba93a
TVSEIMS <karishmawork26@gmail.com>
2026-07-09 14:06:22 +0530

    Add initial terraform config

---
+++ environments/dev/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg_Infra_Automation_Dev"
+    storage_account_name = "tvseinfradev"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-terraform-test"
+  location = "centralindia"
+}
