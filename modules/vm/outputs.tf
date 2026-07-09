6856ccb559ba6f3defd4a91685d6bc727ec52183
TVSEIMS <karishmawork26@gmail.com>
2026-07-09 15:01:37 +0530

    Add outputs for VM ID, private IP, and name

---
+++ modules/vm/outputs.tf
@@ -0,0 +1,14 @@
+output "vm_id" {
+  description = "The resource ID of the created VM"
+  value       = azurerm_linux_virtual_machine.vm.id
+}
+
+output "vm_private_ip" {
+  description = "The private IP address assigned to the VM"
+  value       = azurerm_network_interface.nic.private_ip_configuration[0].private_ip_address
+}
+
+output "vm_name" {
+  description = "The name of the created VM"
+  value       = azurerm_linux_virtual_machine.vm.name
+}
